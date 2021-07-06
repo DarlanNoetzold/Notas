2203ad254bf9d845bdadc910b440c20faf92def6
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2021-07-06 08:08:25 -0300

    request test

--- Backend/src/controllers/AnnotationsController.js
+++ Backend/src/controllers/AnnotationsController.js
@@ -3,6 +3,12 @@ const Annotations = require('../models/AnnotationData');
 
 module.exports = {
 
+    read(request, response){
+        return response.json({
+            sucess: true,
+        })
+    },
+
     create(request, responde){
         const {title, notes, priority} = request.body;
 
--- Backend/src/routes.js
+++ Backend/src/routes.js
@@ -5,6 +5,6 @@ const routes = express.Router();
 const AnnotationsController = require('./controllers/AnnotationsController');
 
 routes.post('/annotations', AnnotationsController.create);
-
+routes.get('/annotations', AnnotationsController.read);
 
 module.exports = routes;

38d38671e8ef9517e2431b301c398002b44f41cb
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2021-07-12 10:42:29 -0300

    Initial React

--- Front/frontend/src/App.js
+++ Front/frontend/src/App.js
@@ -1,25 +1,31 @@
-import React, {useState} from 'react';
-
-import Header from './Header';
+import React from 'react';
+import './global.css';
 
 
 function App() {
-  const [counter, setCounter] = useState(0);
-
-  function handleSoma(){
-    setCounter(counter + 1);
-  }
-
-  function handleSub(){
-    setCounter(counter - 1);
-  }
-
   return (
-    <>
-    <Header title={counter} />
-    <button onClick={handleSoma}>+</button>
-    <button onClick={handleSub}>-</button>
-    </>
+    <div id="app">
+      <aside>
+        <strong>Caderno de Notas</strong>
+        <form>
+
+          <div className="input-block">
+            <label htmlFor="title">Titulo da Anota??o</label>
+            <input></input>
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="nota">Anota??o</label>
+            <textarea></textarea>
+          </div>
+
+          <button type="submit">Salvar</button>
+        </form>
+      </aside>
+      <main>
+
+      </main>
+    </div>
   );
 }
 
--- Front/frontend/src/Header.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function Header(props){
-    return (
-      <h1>header {props.title}</h1>
-    );
-}
-
-export default Header;

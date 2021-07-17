5e5a493bbc1a03d632cc6107b9e193a7c03fa854
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2021-07-17 18:02:42 -0300

    Responsividade

--- Front/frontend/src/Components/Notes/index.js
+++ Front/frontend/src/Components/Notes/index.js
@@ -12,7 +12,7 @@ function Notes(){
               </div>
             </div>
 
-            <textarea>sdasd ifhuas od ghfsdof fgh aods hgop?ashgp udeshgpu dhfg</textarea>
+            <textarea defaultValue="sadjfopijfpoas dasfjajdf p0ajsd sadjf ájsd f´jasd fjas´djf´saif"></textarea>
             <span>!</span>
           </li>
 

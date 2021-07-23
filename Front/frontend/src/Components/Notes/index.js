f8e08211b8dc98137c883d5fb0a0a566af42e332
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2021-07-23 08:23:40 -0300

    Style update

--- Front/frontend/src/Components/Notes/index.js
+++ Front/frontend/src/Components/Notes/index.js
@@ -8,11 +8,24 @@ import api from '../../services/api'
 function Notes({ data }){
     const [ changedNote, setChangedNote ] = useState('');
 
-    async function handlerSave(e, notes){
+    async function handleSave(e, notes){
+      e.style.cursor = 'default';
+      e.style.boxShadow = 'none';
+
       if((changedNote && changedNote != notes)){
         await api.post(`/contents/${data._id}`, {
           notes: changedNote,
         })
+      }
+    }
+
+    function handleEdit(e, priority){
+      e.style.cursor = 'text';
+      e.style.borderRadius = '5px';
+      if(priority){
+        e.style.boxShadow = '0 0 5px white';
+      }else{
+        e.style.boxShadow = '0 0 5px gray';
       }
     }
 
@@ -29,7 +42,8 @@ function Notes({ data }){
             <textarea 
               defaultValue={ data.notes }
               onChange={e => setChangedNote(e.target.value)}
-              onBlur={e => handlerSave(e.target, data.notes)}
+              onClick={e => handleEdit(e.target, data.priority)}
+              onBlur={e => handleSave(e.target, data.notes)}
             />
             <span>
               <AiOutlineExclamationCircle size="20" />
